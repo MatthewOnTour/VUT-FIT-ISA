8e765e6cd832b3df9b15bc016fae535fdfbf64d0
Matúš <m.justik@hotmail.com>
2022-10-21 16:26:46 +0200

    fix parsing bug with pointers

--- sender/dns_sender.c
+++ sender/dns_sender.c
@@ -5,9 +5,9 @@
 #include <netinet/in.h>
 
             /****function declarations****/
-int argvs(int argc, char *argv[] ,bool ipShow, bool srcPath, int ipNum);
+int argvs(int argc, char *argv[] ,bool *ipShow, bool *srcPath, int *ipNum);
 int isValidIp4 (char *str);
-int positioning (bool ipShow, bool srcPath, int ipNum, int baseNum, int dstNum, int srcNum);
+int positioning (bool *ipShow, bool *srcPath, int *ipNum, int *baseNum, int *dstNum, int *srcNum);
         /****end of function declarations****/
 
 /*****main function*****/
@@ -23,49 +23,50 @@ int main(int argc, char *argv[]){
     int srcNum = 0;         //position of [SRC_FILEPATH]
         /****end of flags and intgrs****/
 
-    argvs(argc, argv, ipShow, srcPath, ipNum);
-    positioning(ipShow, srcPath, ipNum, baseNum, dstNum, srcNum);
-
+    argvs(argc, argv, &ipShow, &srcPath, &ipNum);
+    positioning(&ipShow, &srcPath, &ipNum, &baseNum, &dstNum, &srcNum);
+    printf("Hello, World!");
+    return 0;
 }
 /*****end of main function*****/
 
 /****functions****/
 
-int positioning (bool ipShow, bool srcPath, int ipNum, int baseNum, int dstNum, int srcNum){
+int positioning (bool *ipShow, bool *srcPath, int *ipNum, int *baseNum, int *dstNum, int *srcNum){
     if(ipShow){
-        switch (ipNum){
-        case '3':
-            baseNum = 4;
-            dstNum = 5;
-            srcNum = 6;
+        switch (*ipNum){
+        case 3:
+            *baseNum = 4;
+            *dstNum = 5;
+            *srcNum = 6;
             break;
-        case '4':
-            baseNum = 2;
-            dstNum = 5;
-            srcNum = 6;
+        case 4:
+            *baseNum = 2;
+            *dstNum = 5;
+            *srcNum = 6;
             break;
-        case '5':
-            baseNum = 2;
-            dstNum = 3;
-            srcNum = 6;
+        case 5:
+            *baseNum = 2;
+            *dstNum = 3;
+            *srcNum = 6;
             break;
-        case '6':
-            baseNum = 2;
-            dstNum = 3;
-            srcNum = 4;
+        case 6:
+            *baseNum = 2;
+            *dstNum = 3;
+            *srcNum = 4;
             break;
         default:
             fprintf(stderr, "Internal error.\n");
             return(1);  //error
         }
     }else{
-        baseNum = 2;
-        dstNum = 3;
-        srcNum = 4;
+        *baseNum = 2;
+        *dstNum = 3;
+        *srcNum = 4;
     }
 }
 
-int argvs(int argc, char *argv[], bool ipShow, bool srcPath, int ipNum){
+int argvs(int argc, char *argv[], bool *ipShow, bool *srcPath, int *ipNum){
     bool found = false;
 
     if (argc < 3 || argc > 6){
@@ -74,34 +75,34 @@ int argvs(int argc, char *argv[], bool ipShow, bool srcPath, int ipNum){
         return(1);  //error
     }
 
-    for (int a = 0; argv[a] != NULL; a++){
+    for (int a = 1; a < 6; a++){
         if (strcmp(argv[a] , "-u") == 0){
-            ipShow = true;
+            *ipShow = true;
             found = true;
             
             //check ip if it is correct
             isValidIp4(argv[a+1]);
 
             if (argc == 5){             //if argc == 5 there is not [SRC_FILEPATH] of 6 we do have it
-                srcPath = false;
+                *srcPath = false;
             }else if(argc == 6){
-                srcPath = true;
+                *srcPath = true;
             }else{
                 fprintf(stderr, "Invalid input in arguments.\n");
                 fprintf(stderr, "Usage: dns_sender [-u UPSTREAM_DNS_IP] {BASE_HOST} {DST_FILEPATH} [SRC_FILEPATH].\n");
                 return(1);  //error
             }
-            ipNum = a+1;
+            *ipNum = a+1;
         }
     }
 
     if (found == false){
-        ipShow = false;
+        *ipShow = false;
             
         if (argc == 3){         //if argc == 3 there is not [SRC_FILEPATH] of 4 we do have it
-            srcPath = false;
+            *srcPath = false;
         }else if(argc == 4){
-            srcPath = true;
+            *srcPath = true;
         }else{
             fprintf(stderr, "Invalid input in arguments.\n");
             fprintf(stderr, "Usage: dns_sender [-u UPSTREAM_DNS_IP] {BASE_HOST} {DST_FILEPATH} [SRC_FILEPATH].\n");
