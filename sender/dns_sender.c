3a0148c59f1dd518998218e71e16b4c924bfaa81
Matúš <m.justik@hotmail.com>
2022-10-21 14:02:30 +0200

    fix parse problems

--- sender/dns_sender.c
+++ sender/dns_sender.c
@@ -4,6 +4,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+/* function declarations */
+int argvs(int argc, char *argv[] ,bool ipShow, bool srcPath, int ipNum);
 
 
 //main function
@@ -12,25 +14,59 @@ int main(int argc, char *argv[]){
 FILE *fp;
 int c;
 
+//flags
+bool ipShow = false;  //flag for "-u"
+bool srcPath = false;   //falag for [SRC_FILEPATH]
+int ipNum = 0;
+
+argvs(argc, argv, ipShow, srcPath, ipNum);
+
+
+
+}
+
+
+//functions
+int argvs(int argc, char *argv[], bool ipShow, bool srcPath, int ipNum){
+
 if (argc < 3 || argc > 6){
     fprintf(stderr, "Invalid number of arguments.\n");
+    fprintf(stderr, "Usage: dns_sender [-u UPSTREAM_DNS_IP] {BASE_HOST} {DST_FILEPATH} [SRC_FILEPATH].\n");
     return(1);  //error
 }
 
-if (argc == 3){
-    // kontrola argv ci su spravne
-}
+for (int a = 0; argv[a] != NULL; a++){
+    if (strcmp(argv[a] , "-u") == 0){
+        ipShow == true;
+        
+        //check ip if it is correct TODO
 
-if (argc == 4){
-    if (argv[1] == "-u"){ 
-        fprintf(stderr, "Invalid arguments.\n");
-        return(1);  //error
-    }else if(){
-        // check last argv and load if needed
+
+
+        if (argc == 5){             //if 5 argc there is not [SRC_FILEPATH] of 6 we do have it
+            srcPath = false;
+        }else if(argc == 6){
+            srcPath == true;
+        }else{
+            fprintf(stderr, "Invalid input in arguments.\n");
+            fprintf(stderr, "Usage: dns_sender [-u UPSTREAM_DNS_IP] {BASE_HOST} {DST_FILEPATH} [SRC_FILEPATH].\n");
+            return(1);  //error
+        }
+    }else{
+        ipShow = false;
+        
+        if (argc == 3){         //if 3 argc there is not [SRC_FILEPATH] of 4 we do have it
+            srcPath = false;
+        }else if(argc == 4){
+            srcPath == true;
+        }else{
+            fprintf(stderr, "Invalid input in arguments.\n");
+            fprintf(stderr, "Usage: dns_sender [-u UPSTREAM_DNS_IP] {BASE_HOST} {DST_FILEPATH} [SRC_FILEPATH].\n");
+            return(1);  //error
+        }
     }
 }
 
 
 
-    
 }
