798fd443894e4b7a78b7b6b89efbed153fa7e86d
Matúš <m.justik@hotmail.com>
2022-10-23 15:31:06 +0200

    Server side implementation of UDP

--- receiver/dns_receiver.c
+++ receiver/dns_receiver.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
 #include <string.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+#define PORT     8080 
+#define MAXLINE 1024
 
             /****function declarations****/
 int argvs(int argc, char *argv[]);
@@ -12,15 +19,57 @@ int argvs(int argc, char *argv[]);
 /*****main function*****/
 int main(int argc, char *argv[]){
 
-   argvs(argc, argv);
-
+    argvs(argc, argv);
+    //Server side implementation of UDP from https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from server"; 
+    struct sockaddr_in servaddr, cliaddr; 
+        
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+        
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+        
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
+        
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+        
+    int len, n; 
+    
+    len = sizeof(cliaddr);  //len is value/result 
+    
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Client : %s\n", buffer); 
+    sendto(sockfd, (const char *)hello, strlen(hello),  
+        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+            len); 
+    printf("Hello message sent.\n");  
+    
+    
     return 0;
 }
 /*****end of main function*****/
 
 /****functions****/
 int argvs(int argc, char *argv[]){
-    if(argc == 2){
+    if(argc == 3){
         return;
     }else{
         fprintf(stderr, "Invalid number of arguments.\n");
